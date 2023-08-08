20a1b967a127415509c2f6356c1a39920f6de275
James Lee <jameslee@Jamess-MacBook-Pro.local>
2023-08-08 14:07:04 -0700

    countdown js

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,34 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let time = 1500;
+  let countdownInterval;
+
+
+  function countDown() {
+    countdownInterval = setInterval(() => {
+      if (time > 0) {
+        time--;
+        updateTime();
+      } else {
+        clearInterval(countdownInterval);
+      }
+    }, 1000); 
+  }
+
+  function updateTime() {
+    document.getElementById("display").innerHTML = time;
+  }
+
+  function resetTime() {
+    time = 1500;
+    updateTime();
+  }
+
+  function stopTime() {
+    
+  }
+    
 
   return (
     <>
@@ -13,11 +40,13 @@ function App() {
     </div>
 
     <div className='flex justify-center bg-teal-300 '>
-    <h1 className="text-4xl font-bold dark:text-white my-10">25:00</h1>
+    <h1 id="display" className="text-4xl font-bold dark:text-white my-10">{time}</h1>
     </div>
 
     <div className='flex justify-center my-5'>
-    <button className="bg-indigo-600 text-white p-5 rounded-md ">Start Timer</button>
+    <button onClick={countDown} className="bg-indigo-600 text-white p-5 rounded-md mx-5">Start Timer</button>
+    <button className="bg-red-500 text-white p-5 rounded-md mx-5 ">Stop Timer</button>
+    <button onClick={resetTime} className='bg-purple-500 mx-5 text-white p-5 rounded-md'>Reset</button>
     </div>
     </>
   )
